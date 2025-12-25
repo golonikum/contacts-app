1888a35644f68d581d5cc932cadb2916a988d28f
Голов Никита Викторович <n.golov@zetra.pro>
2025-12-25 21:02:59 +0300

    Fix

--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -12,7 +12,8 @@ export const sendEmail = async ({
       Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
     },
     body: JSON.stringify({
-      from: process.env.RESEND_FROM_EMAIL || "Acme <onboarding@resend.dev>",
+      from:
+        process.env.RESEND_FROM_EMAIL || "Contacts App <onboarding@resend.dev>",
       to: [process.env.ADMIN_EMAIL],
       subject,
       html: htmlContent,
